9c6b4282a8589329dbca96be6d954f9d3de9e1e0
Denisse Nova <denisse.nova.v@gmail.com>
2021-11-09 23:35:31 -0300

    final

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-/*let div = document.getElementById('Pokemones')
-  let spinner = document.getElementById('spinner')
-  tch('http://localhost:3000/pokemones')
-  .then((res) => res.json())
-  .then((pokemones) => {
-    console.log(pokemones)
-    spinner.style.display = 'none'
-    pokemones.forEach((p) => {
-      div.innerHTML += `
-    <div>
-      <img src="${p.img}" >
-      <p>${p.nombre}</p>
-    </div>
-  `
-    })
-  })*/
-
+/**/
 const axios = require('axios');
 const http = require ('http');
 
-http.createServer((req, res ) => {
+const PORT = 3000;
 
-}).listen(3000,()=> console.log( 'escuchando puerto 3000'))
+http
+  .createServer(async (req, res ) => {
+    // CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Max-Age', 2592000);
 
+    if (req.url === '/pokemones') {
+      const allPokemon = await getAll();
+
+      const allPokemonData = allPokemon.map((item) => { //map en vez de forech , y lo transforma en lo que quiera en este caso una promesa
+        return getPokemon(item.url)
+      })
+  
+      const data = await Promise.all(allPokemonData);
+  
+      res.write(JSON.stringify(data)) // un objeto o arreglo a texto 
+    }
+    res.end()
+  }).listen(PORT,()=> console.log( `escuchando puerto http://localhost:${PORT}`))
+
+
+
+const getAll = async () => { //obtiene los 150 pokemones con validacion 
+  const result = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=150`).catch(e => e);
+  if (!result || !result.data || !result.status || result.status >= 300) return []
+  return result.data.results;
+}
+
+const getPokemon = async (url) => { // se obtiene datos e la url por un pokemon 
+  const result = await axios.get(url).catch(e => e);
+  if (!result || !result.data || !result.status || result.status >= 300) return;
+  return {
+    image: result.data.sprites.front_default,
+    name: result.data.name,
+    order: result.data.order
+  };
+}
+
+
+/*
 let pokemonesPromesas = []
 
 async function pokemonesGet() {
@@ -45,3 +68,4 @@ let pokemonName = p.name
     })
   }) 
 })
+*/
